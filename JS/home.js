3d13967fcb77f0624b080ae21fc49b5be9740abd
Omar Gomaa <mrjmh934@gmail.com>
2025-08-05 14:46:37 +0300

    finish AdminPage and LoginPage and HomePage

--- JS/home.js
+++ JS/home.js
@@ -7,7 +7,6 @@ async function getTopStudents(){
     let data = await response.json();
     data.forEach(element => {
     let values = Object.values(element)
-    console.log(element)
     let tr = document.createElement('tr');
     values.forEach(e=>{
         let td = document.createElement('td');
@@ -20,9 +19,37 @@ async function getTopStudents(){
 }
 window.onload =  getTopStudents;
 
-
-
-
-
+let logout = document.querySelector('.navbar .container-fluid .collapse ul li:nth-child(2) a')
+let login = document.querySelector('.navbar .container-fluid .collapse ul li:nth-child(1) a')
+let MyPage = document.querySelector('.navbar .container-fluid .collapse ul li:nth-child(5) a')
+let ErrorMessage = document.querySelector('.alert')
+logout.onclick = function(){
+    localStorage.removeItem('id')
+}
+login.onclick = function(ev){
+    ev.preventDefault();
+    if(localStorage.getItem('id')){
+        ErrorMessage.innerText = `لتسجيل الدخول بحساب أخر يجب تسجيل الخروج أولا`
+        ErrorMessage.classList.remove('d-none')
+        setTimeout(()=>{
+            ErrorMessage.classList.add('d-none')
+        },4000)
+    }else{
+            document.location.href = '/Html/Login.html';
+    }
+}
+MyPage.onclick = function(ev){
+    ev.preventDefault();
+    if(!localStorage.getItem('id')){
+        ErrorMessage.innerText=`يجب أن تقوم بتسجيل الدخول أولا لعرض صفحتك`
+        ErrorMessage.classList.remove('d-none')
+        setTimeout(()=>{
+            ErrorMessage.classList.add('d-none')
+        },4000)
+    }else{
+        console.log('MyPage')
+            // document.location.href = '/Html/Login.html';
+    }
+}
 
 
